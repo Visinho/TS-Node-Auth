115f0122070a0b69746263471cb931339bed0c7e
Visinho <Elvisvisinho@rocketmail.com>
2024-04-30 10:56:02 +0100

    Creation of the validateResource middleware for validating user input

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import validateResource from "../middlewares/validateResource";
+import { createUserSchema } from "../schema/user.schema";
+import { createUserHandler } from "../controllers/user.controller";
 
 const router = express.Router();
 
-router.post("/api/users", (req, res) => res.sendStatus(200));
+router.post("/api/users", validateResource(createUserSchema), createUserHandler);
 
 export default router;
