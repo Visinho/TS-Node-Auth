334f986926b06428631e192af849afbceffa6b0e
Visinho <Elvisvisinho@rocketmail.com>
2024-04-30 12:58:39 +0100

    Creation of the user and email messaging using nodemailer

--- config/default.ts
+++ config/default.ts
@@ -6,5 +6,12 @@ dotenv.config();
 export default {
     port: 3000,
     dbUri: process.env.MONGO_URI,
-    logLevel: "info"
+    logLevel: "info",
+    smtp: {
+        user: 'ounup4zotlulfbup@ethereal.email',
+        pass: 'PMepRpUK8C6UfESJxp',
+        host: "smtp.ethereal.email",
+        port: 587,
+        secuure: false
+    }
 };
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,11 +1,19 @@
 import { Request, Response } from "express";
 import { CreateUserInput } from "../schema/user.schema";
 import { createUser } from "../services/user.service";
+import sendEmail from "../utils/mailer";
 
 export async function createUserHandler(req: Request<{}, {}, CreateUserInput>, res: Response) {
     const body = req.body
     try {
         const user = await createUser(body);
+
+        await sendEmail({
+            from: "Elvisvisinho97@gmail.com",
+            to: user.email,
+            subject: "Please verify your account",
+            text: `Verification code ${user.verificationCode}, Id: ${user._id}`
+        });
         return res.send("User successfully Created")
     } catch (e: any) {
         if(e.code === 11000) {
