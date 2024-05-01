22a736fee18f4c687cc76256e063080f9aeef91d
Visinho <Elvisvisinho@rocketmail.com>
2024-05-01 10:29:14 +0100

    Creation of the verify user logic

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import { CreateUserInput } from "../schema/user.schema";
-import { createUser } from "../services/user.service";
+import { CreateUserInput, VerifyUserInput } from "../schema/user.schema";
+import { createUser, findUserById } from "../services/user.service";
 import sendEmail from "../utils/mailer";
 
 export async function createUserHandler(req: Request<{}, {}, CreateUserInput>, res: Response) {
@@ -21,4 +21,31 @@ export async function createUserHandler(req: Request<{}, {}, CreateUserInput>, r
         }
         return res.status(500).send(e);
     }    
+}
+
+export async function verifyUserHandler(req: Request<VerifyUserInput>, res: Response) {
+    const id = req.params.id;
+    const verificationCode = req.params.verificationCode
+
+    //Find user by Id
+    const user = await findUserById(id);
+
+    if(!user) {
+        return res.send("User not found!")
+    }
+
+    //Check to see if they are already verified
+    if(user.verified) {
+        return res.send("User is already verified!")
+    }
+
+    //Check to see if the verificationCode matches
+    if(user.verificationCode === verificationCode) {
+        user.verified = true;
+        await user.save();
+        return res.send("User successfully verified");
+    }
+        return res.send("Could not verify user!")
+
+    
 }
